03fd8c908102303cf50d5c79978e380bc4f8c635
dev israel <devisrael01@gmail.com>
2024-04-08 16:30:52 +0100

    added another firestore db

--- src/config/firbase.ts
+++ src/config/firbase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZyOGfrPCUgLyWvabG_obRSAugCjQyvEg",
-  authDomain: "spotta-6aa03.firebaseapp.com",
-  projectId: "spotta-6aa03",
-  storageBucket: "spotta-6aa03.appspot.com",
-  messagingSenderId: "198962450798",
-  appId: "1:198962450798:web:e37790ebd2eaee24ddf17f"
+  apiKey: "AIzaSyBSe3fHiZwwj3uvI9t79h6l9HYE4jLh1js",
+  authDomain: "spotta-9721f.firebaseapp.com",
+  projectId: "spotta-9721f",
+  storageBucket: "spotta-9721f.appspot.com",
+  messagingSenderId: "825075792293",
+  appId: "1:825075792293:web:68f19696cea3b50fb9d04c"
 };
 
 export const app = initializeApp(firebaseConfig);
