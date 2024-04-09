efc4f61629e52ea402e357872f261ddb14690c4f
dev israel <devisrael01@gmail.com>
2024-04-09 07:54:46 +0100

    added api keys to env file

--- src/config/firbase.ts
+++ src/config/firbase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBSe3fHiZwwj3uvI9t79h6l9HYE4jLh1js",
-  authDomain: "spotta-9721f.firebaseapp.com",
-  projectId: "spotta-9721f",
-  storageBucket: "spotta-9721f.appspot.com",
-  messagingSenderId: "825075792293",
-  appId: "1:825075792293:web:68f19696cea3b50fb9d04c"
+  apiKey: process.env.NEXT_APP_API_KEY,
+  authDomain: "spotta-b129f.firebaseapp.com",
+  projectId: "spotta-b129f",
+  storageBucket: "spotta-b129f.appspot.com",
+  messagingSenderId: "536686903228",
+  appId: "1:536686903228:web:8b153eac85c931afa5b5b9"
 };
 
 export const app = initializeApp(firebaseConfig);
